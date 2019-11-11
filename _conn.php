74bdc0d055bd363612fdc898aeb49eea7f8f6aee
sleepycow <sleepycc@vip.qq.com>
2019-11-11 16:38:42 +0800

    更新版本

--- _conn.php
+++ _conn.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 date_default_timezone_set("PRC");
 define("EMS_NAME","爱尔市场部筛查组",FALSE);
-define("EMS_VERSION","1.0",FALSE);
+define("EMS_VERSION","2.0",FALSE);
 require_once ("lib/Medoo.php");
 use Medoo\Medoo;
 /*$db = new Medoo([
